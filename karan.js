f1c09f1c3a6cc99f5fdf0601948c34cc383b44bd
Karan Sagar <ksagar1030@gmail.com>
2012-05-30 14:03:01 -0400

    added method and integer from jsgood

--- karan.js
+++ karan.js
@@ -1,3 +1,4 @@
+//adds a 'create' method to all objects
 if (typeof Object.create !== 'function') {
      Object.create = function (o) {
          var F = function () {};
@@ -5,3 +6,18 @@ if (typeof Object.create !== 'function') {
          return new F();
      };
 }
+
+//adds a 'method' method to all methods that let
+//them add methods to their linked 'prototype'
+//object e.g., the property 'myfunc.prototype'
+Function.prototype.method = function (name, func) {
+        this.prototype[name] = func;
+        return this;
+};
+
+//adds method .integer() to all numbers that returns integer part
+Number.method('integer', function (  ) {
+        return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+
