959a03e877c5be4b680ff446685b8036275b38b0
Karan Sagar <ksagar1030@gmail.com>
2012-05-31 16:46:30 -0400

    added curry

--- karan.js
+++ karan.js
@@ -11,8 +11,10 @@ if (typeof Object.create !== 'function') {
 //them add methods to their linked 'prototype'
 //object e.g., the property 'myfunc.prototype'
 Function.prototype.method = function (name, func) {
+    if(!this.prototype[name]){
         this.prototype[name] = func;
         return this;
+    }
 };
 
 //adds method .integer() to all numbers that returns integer part
@@ -20,4 +22,17 @@ Number.method('integer', function (  ) {
         return Math[this < 0 ? 'ceil' : 'floor'](this);
 });
 
+//trims the whitespace off the end of a string
+String.method('trim', function (  ) {
+        return this.replace(/^\s+|\s+$/g, '');
+});
 
+
+Function.method('curry', function (  ) {
+        var slice = Array.prototype.slice,
+            args = slice.apply(arguments),
+            that = this;
+    return function (  ) {
+                return that.apply(null, args.concat(slice.apply(arguments)));
+                    };
+});
